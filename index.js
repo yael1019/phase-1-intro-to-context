2a28059ad206d8c1daffef7c7388757169a352f1
Yael Harosh <yaelharosh01@gmail.com>
2023-03-01 14:04:10 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const createEmployeeRecord = arr => {
+    const timeInEvents = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(i === 0) {
+            const firstName = arr[i];
+        } else if(i === 1) {
+            const familyName = arr[i];
+        } else if(i === 2) {
+            const title = arr[i];
+        } else if(i === 3) {
+            const payPerHour = arr[i];
+        } 
+    }
+}
